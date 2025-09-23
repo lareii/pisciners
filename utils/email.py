19f867f7b7d152a7dd77540d31954497d412da35
lareii <lareii@proton.me>
2025-09-23 23:32:24 +0300

    refactor: update email sending logic and improve message formatting

--- utils/email.py
+++ utils/email.py
@@ -1,4 +1,10 @@
-from config import SENDER_ADDR, SENDER_PASS, RECEIVER_ADDRS
+from config import (
+    SENDER_ADDR,
+    SENDER_PASS,
+    RECEIVER_ADDRS,
+    SMTP_SERVER,
+    SMTP_PORT
+)
 
 from email.mime.text import MIMEText
 import smtplib
@@ -9,10 +15,10 @@ def send_emails(pisciner, project_slug):
         f"""<html> <body><img src="{pisciner['image']['link']}" width="300" height="225"><br><small>{pisciner['login']} ({pisciner['usual_full_name']})</small><p><b>{pisciner['login']}</b>, <b>{project_slug}</b> projesini <b>{pisciner['location']}</b> lokasyonunda gönderdi.</p></body></html>""",
         "html",
     )
-    msg["Subject"] = "proje veren var tutorum"
-    msg["From"] = f"emirhan <{SENDER_ADDR}>"
+    msg["Subject"] = f"{pisciner['login']}, {project_slug} projesini gönderdi!"
+    msg["From"] = f"pisciners-bot <{SENDER_ADDR}>"
     msg["To"] = ", ".join(RECEIVER_ADDRS)
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465) as smtp_server:
+    with smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT) as smtp_server:
         smtp_server.login(SENDER_ADDR, SENDER_PASS)
         smtp_server.sendmail(SENDER_ADDR, RECEIVER_ADDRS, msg.as_string())
